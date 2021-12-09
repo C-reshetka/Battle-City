96d7da8be3c228e18ff7cd25877019e6bc80e51d
Ivan <krash577388@mail.ru>
2021-12-09 16:03:03 +0500

    Player

--- Player.py
+++ Player.py
@@ -1,2 +1,36 @@
 import pygame as pg
 
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.player_sprite_right = pg.transform.scale(pg.image.load('tmp/pl_right.png'), (25, 25))
+        self.player_sprite_left = pg.transform.scale(pg.image.load('tmp/pl_left.png'), (25, 25))
+        self.player_sprite_up = pg.transform.scale(pg.image.load('tmp/pl_up.png'), (25, 25))
+        self.player_sprite_down = pg.transform.scale(pg.image.load('tmp/pl_down.png'), (25, 25))
+        self.image = self.player_sprite_up
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.x = self.rect.x
+        self.y = self.rect.y
+        self.is_left, self.is_right, self.is_down, self.is_up = 0, 0, 0, 1
+        self.width, self.height = 25, 25
+
+    def move(self, delta_x, delta_y):
+        if delta_x > 0:
+            self.rect.x += delta_x
+            self.image = self.player_sprite_right
+            self.is_left, self.is_right, self.is_down, self.is_up = 0, 1, 0, 0
+        elif delta_x < 0:
+            self.rect.x += delta_x
+            self.image = self.player_sprite_left
+            self.is_left, self.is_right, self.is_down, self.is_up = 1, 0, 0, 0
+        elif delta_y > 0:
+            self.rect.y += delta_y
+            self.image = self.player_sprite_down
+            self.is_left, self.is_right, self.is_down, self.is_up = 0, 0, 1, 0
+        elif delta_y < 0:
+            self.rect.y += delta_y
+            self.image = self.player_sprite_up
+            self.is_left, self.is_right, self.is_down, self.is_up = 0, 0, 0, 1
--- map.py
+++ map.py
@@ -2,6 +2,7 @@ import pygame
 
 from Projectile import Projectile
 from Wall import Wall
+from Player import Player
 
 pygame.init()
 game_window_width = 500
@@ -11,25 +12,18 @@ pygame.display.set_caption("Battle City")
 
 clock = pygame.time.Clock()
 
-player_sprite_right = pygame.transform.scale(pygame.image.load('tmp/pl_right.png'), (25, 25))
-player_sprite_left = pygame.transform.scale(pygame.image.load('tmp/pl_left.png'), (25, 25))
-player_sprite_up = pygame.transform.scale(pygame.image.load('tmp/pl_up.png'), (25, 25))
-player_sprite_down = pygame.transform.scale(pygame.image.load('tmp/pl_down.png'), (25, 25))
-
 background_sprite = pygame.transform.scale(pygame.image.load('tmp/bg.png'), (game_window_width, game_window_height))
 
-player_x = 50
-player_y = 50
-player_width = 25
-player_height = 25
 player_speed = 1
 
-is_left, is_right, is_down, is_up = 0, 0, 0, 1
+player = Player(50, 50)
 
 is_running = True
 
 sprites = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
+
+sprites.add(player)
 walls = pygame.sprite.Group()
 wall = Wall(0, 0)
 sprites.add(wall)
@@ -46,29 +40,27 @@ while is_running:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_UP]:
-        player_y -= player_speed if player_y - player_speed >= 0 else player_y
-        is_left, is_right, is_down, is_up = 0, 0, 0, 1
+        if player.rect.y - player_speed >= 0:
+            player.move(0, -player_speed)
     elif keys[pygame.K_DOWN]:
-        if player_y + player_speed + player_height <= game_window_height:
-            player_y += player_speed
-        is_left, is_right, is_down, is_up = 0, 0, 1, 0
+        if player.rect.y + player_speed + player.height <= game_window_height:
+            player.move(0, player_speed)
     elif keys[pygame.K_RIGHT]:
-        if player_x + player_speed <= game_window_width - player_width:
-            player_x += player_speed
-        is_left, is_right, is_down, is_up = 0, 1, 0, 0
+        if player.rect.x + player_speed + player.width <= game_window_width:
+            player.move(player_speed, 0)
     elif keys[pygame.K_LEFT]:
-        player_x -= player_speed if player_x - player_speed >= 0 else player_x
-        is_left, is_right, is_down, is_up = 1, 0, 0, 0
+        if player.rect.x - player_speed >= 0:
+            player.move(-player_speed, 0)
     elif keys[pygame.K_SPACE]:
         if bullets_pixel_interval_count > bullets_interval:
-            if is_left:
-                p = Projectile(player_x - 0.5 * player_width, player_y + 0.2 * player_height, -1, 0)
-            elif is_right:
-                p = Projectile(player_x + player_width, player_y + 0.2 * player_height, 1, 0)
-            elif is_up:
-                p = Projectile(player_x + 0.25 * player_width, player_y - 0.7 * player_height, 0, -1)
-            elif is_down:
-                p = Projectile(player_x + 0.25 * player_width, player_y + player_height, 0, 1)
+            if player.is_left:
+                p = Projectile(player.rect.x - 0.5 * player.width, player.rect.y + 0.2 * player.height, -1, 0)
+            elif player.is_right:
+                p = Projectile(player.rect.x + player.width, player.rect.y + 0.2 * player.height, 1, 0)
+            elif player.is_up:
+                p = Projectile(player.rect.x + 0.25 * player.width, player.rect.y - 0.7 * player.height, 0, -1)
+            elif player.is_down:
+                p = Projectile(player.rect.x + 0.25 * player.width, player.rect.y + player.height, 0, 1)
             bullets.add(p)
             sprites.add(p)
             bullets_pixel_interval_count = 0
@@ -76,19 +68,10 @@ while is_running:
     game_window.blit(background_sprite, (0, 0))
     bullets_pixel_interval_count += 1
 
-    player_coordinates = (player_x, player_y)
-    if is_up:
-        game_window.blit(player_sprite_up, player_coordinates)
-    elif is_down:
-        game_window.blit(player_sprite_down, player_coordinates)
-    elif is_right:
-        game_window.blit(player_sprite_right, player_coordinates)
-    elif is_left:
-        game_window.blit(player_sprite_left, player_coordinates)
-
     for e in sprites:
         game_window.blit(e.image, (e.rect.x, e.rect.y))
     bullets.update()
 
     pygame.display.update()
+
 pygame.quit()
